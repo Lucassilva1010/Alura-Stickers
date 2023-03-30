727e68af0da81fdd34df3a9a569d0e4c2e9783ff
Lucas Augusto <lucas_lapada@hotmail.com>
2023-03-30 02:46:45 -0300

    Aplicações de POO e API da NASA - OK

---
+++ src/Conteudo.java
@@ -0,0 +1,28 @@
+import java.util.Properties;
+
+public class Conteudo {
+    
+    private String tiulo;
+    private String urlImagem;
+
+
+
+    
+    public Conteudo(String tiulo, String urlImagem) {
+        this.tiulo = tiulo;
+        this.urlImagem = urlImagem;
+    }
+
+    public String getTiulo() {
+        return tiulo;
+    }
+    
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+    
+    
+   
+
+    
+}
